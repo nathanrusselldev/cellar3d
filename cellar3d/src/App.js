9d21b6212f22ee3d3c1c68dc9ec7514c088ce6df
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-07 10:24:48 -0400

    added basic routing from the react level for the application so we can have single page functionality, added the made export for the homepage function

--- cellar3d/src/App.js
+++ cellar3d/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Homepage from './components/homepage/homePage';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={`REPLACE WITH PAGE RENDER OBJECT`} />
+          <Route path="/signUp" element={`REPLACE WITH PAGE RENDER OBJECT`} />
+          <Route path="/profile" element={`REPLACE WITH PAGE RENDER OBJECT`} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
