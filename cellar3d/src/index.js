e8c5487512bae79294000d6bd998d42bd7a5180f
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-07 10:18:43 -0400

    Added the skeleton for the comp file along with subfolders for additional pages that will be included in the application

--- cellar3d/src/index.js
+++ cellar3d/src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -8,7 +7,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.querySelector(`main`)
 );
 
 // If you want to start measuring performance in your app, pass a function
