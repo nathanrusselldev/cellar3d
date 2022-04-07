2573c1cb86f9afaaf45f9c67f3617f8d1be4dc1f
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-07 14:21:23 -0400

    Re created the package.json files so the client can be launched while still being able to be connected to the apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import DevNav from './components/devNav/devNav';
@@ -9,25 +9,25 @@ import ProfilePage from './components/profile/profilePage';
 import CellarPage from './components/profile/cellar/cellarPage';
 
 
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    // <ApolloProvider client={client}>
-    <Router>
-      <DevNav />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signUp" element={<SignUpPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/profile/cellar" element={<CellarPage />} />
-      </Routes>
-    </Router>
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <Router>
+        <DevNav />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signUp" element={<SignUpPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/cellar" element={<CellarPage />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  db.sync({force:false}).then(() => {
+  db.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
