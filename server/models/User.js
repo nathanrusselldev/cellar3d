ed39a487ffdd5b60ef4d3520b7cbbe6d5d62f255
nathanrusselldev <nathanrussell.it@gmail.com>
2022-04-08 12:20:22 -0400

    even better password checker

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const sequelize = require ('../config/connection');
 const bcrypt = require('bcrypt')
 
 class User extends Model {
-  checkPassword(loginPw) {
+  isCorrectPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
