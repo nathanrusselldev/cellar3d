b3829b673663ffea2ad1bced765c54798ca6cd3d
nathanrusselldev <nathanrussell.it@gmail.com>
2022-04-08 11:39:55 -0400

    login token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Bottle, Cellar } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -9,9 +10,23 @@ const resolvers = {
   },
   Mutation: {
     createUser: async (parent, args, context, info) => {
-      const user = await User.create(args); console.log(user)
+      const user = await User.create(args);
       const cellar = await Cellar.create({userId: user.id, })
+      const token = signToken(user);
       return await User.findByPk (user.id, {include: [{model: Cellar}]})
+    },
+    login: async (parent, {username, password}) => {
+      const user = await User.findOne({ username })
+
+      if(!user) {
+        throw new AuthenticationError('Incorrect username or password.')
+      }
+      const correctPassword = await user.isCorrectPassword(password)
+      if(!correctPassword) {
+        throw new AuthenticationError('Incorrect username or password.');
+      }
+      const token = signToken(user);
+      return {token, user}
     }
   }
 }
