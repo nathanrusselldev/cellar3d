eee455a2f96b127ebe72cabb237a2b5944d6f68b
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-08 12:07:46 -0400

    Added the state functionality for the sign in for the front end, aditionally added the correspondiong mutation for sign in

--- client/src/components/login/loginPage.jsx
+++ client/src/components/login/loginPage.jsx
@@ -1,43 +1,44 @@
+import { useMutation } from "@apollo/client"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import Auth from "../../../../server/utils/auth"
+import { LOGIN } from '../../utils/mutations'
 
 function LoginPage(){
-/*
-        const [formState, setFormState] = useState({ email: '', password: '' });
-        const [login, { error }] = useMutation(LOGIN);
-    
-        const handleFormSubmit = async (event) => {
-            event.preventDefault();
-            try {
-                const mutationResponse = await login({
-                    variables: { email: formState.email, password: formState.password },
-                });
-                const token = mutationResponse.data.login.token;
-                Auth.login(token);
-            } catch (e) {
-                console.log(e);
-            }
-        };
-    
-        const handleChange = (event) => {
-            const { name, value } = event.target;
-            setFormState({
-                ...formState,
-                [name]: value,
-            });
-        };
-*/
 
-        
+    const [formState, setFormState] = useState({username: ``, password:``})
+    const [login, {error}] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables : {username: formState.username, password: formState.password}
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormState({
+            ...formState,
+            [name]:value,
+        });
+    };
 
     return (
         <section className="loginPage">
             <h2>Login!</h2>
             <p>Happy to see you again</p>
-            <form id="signInForm">
-                <label htmlFor="usernameOrEmail">Username / Email Address</label>
-                <input type="text" name="usernameOrEmail" required="required" placeholder="Username or Email"/>
+            <form id="signInForm" onSubmit={handleFormSubmit}>
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" required="required" placeholder="Username or Email"  handleChange={handleChange} />
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" required="required" placeholder="password"/>
+                <input type="password" name="password" required="required" placeholder="password" handleChange={handleChange} />
                 <input type="submit" value="Log In" />
             </form>
             <p>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,13 +2,25 @@ import { gql } from '@apollo/client'
 
 
 export const ADD_USER = gql`
-mutation Mutation($username: String!, $email: String!, $firstName: String!, $lastName: String!, $password: String!) {
+mutation createUser($username: String!, $email: String!, $firstName: String!, $lastName: String!, $password: String!) {
     createUser(username: $username, email: $email, first_name: $firstName, last_name: $lastName, password: $password) {
         id
         username
         email
         first_name
         last_name
+    }
+}
+`
+
+export const LOGIN = gql`
+mutation login($username:String!, $password:String!) {
+    login(username:$username, password:$password) {
+        token
+        user {
+            id
+            username
+        }
     }
 }
 `
