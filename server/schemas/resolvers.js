d4d4671f06fbacb043d04832e0165821179debd0
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-11 10:45:55 -0400

    Saving before a merge

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,34 +8,34 @@ const resolvers = {
       return User.find().populate('bottle');
     },
     me: async (parent, args, context) => {
-      if(context.user) {
-        return User.findOne({id: context.user.id})
+      if (context.user) {
+        return User.findOne({ id: context.user.id })
       }
     }
   },
   Mutation: {
     createUser: async (parent, args, context, info) => {
       const user = await User.create(args);
-      const cellar = await Cellar.create({userId: user.id, })
-      const token = signToken({...user, cellarId: cellar.id});
-      return {user: await User.findByPk (user.id, {include: [{model: Cellar}]}), token}
+      const cellar = await Cellar.create({ userId: user.id, })
+      const token = signToken({ ...user, cellarId: cellar.id });
+      return { user: await User.findByPk(user.id, { include: [{ model: Cellar }] }), token }
     },
-    login: async (parent, {username, password}) => {
+    login: async (parent, { username, password }) => {
       const user = await User.findOne({ username },
-        {include: [{ model: Cellar }]})
-      
-      if(!user) {
+        { include: [{ model: Cellar }] })
+      console.log(user);
+      if (!user) {
         throw new AuthenticationError('Incorrect username or password.')
       }
       const correctPassword = await user.isCorrectPassword(password)
-      if(!correctPassword) {
+      if (!correctPassword) {
         throw new AuthenticationError('Incorrect username or password.');
       }
-      const token = signToken({...user, cellarId: user.cellar.id});
-      return {token, user}
+      const token = signToken({ ...user, cellarId: user.cellar.id });
+      return { token, user }
     },
-    createBottle: async(parent, args, context) => {
-      const newbottle = await Bottle.create({...args, userId: context.user.id, cellarId: context.user.cellarId})
+    createBottle: async (parent, args, context) => {
+      const newbottle = await Bottle.create({ ...args, userId: context.user.id, cellarId: context.user.cellarId })
       return newbottle
     }
   }
